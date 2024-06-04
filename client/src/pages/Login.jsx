c83ccaaf5b9f4beb1faf24a9012c187e5b1334f5
Akhilesh <choudharyakhilesh2003@gmail.com>
2024-06-04 23:41:35 +0530

    1 - Create Login and Register Page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function Login() {
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    })
+    const loginUser = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <div>
+            <form onSubmit={loginUser}>
+                <label>Email</label>
+                <input
+                    type="email"
+                    placeholder='Email'
+                    value={data.email}
+                    onChange={(e) => setData({ ...data, email: e.target.value })}
+                />
+                <label>Password</label>
+                <input
+                    type="password"
+                    placeholder='Password'
+                    value={data.password}
+                    onChange={(e) => setData({ ...data, password: e.target.value })}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
